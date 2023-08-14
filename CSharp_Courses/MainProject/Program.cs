4b1ef899f02f98570e932ee7a96db50f5e05b93f
VictorMauroy <mauroy.victor@gmail.com>
2023-08-14 16:36:56 +0200

    build: :tada: create a new solution with an empty console projet for future C# lessons examples

---
+++ CSharp_Courses/MainProject/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MainProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    /* N'hésitez pas à créer d'autres projets dans cette solution si vous souhaitez développer des exemples
+     * plus poussés pour certains concepts.
+     * 
+     * Vous pouvez aussi simplement créer des classes exemple dans ce projet console.
+     */
+}
